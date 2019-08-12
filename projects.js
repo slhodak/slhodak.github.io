c0f8f3f4e01c5a367c78905df6d3381153d73113
Sam Hodak <slhodak@gmail.com>
2019-08-12 14:27:41 -0700

    Add tic-tac-toe

--- projects.js
+++ projects.js
@@ -7,5 +7,14 @@ export default [
     },
     image: "midi-key.png",
     link: "https://slhodak.github.io/WebSynth/dist"
+  },
+  {
+    title: "Tic-Tac-Toe",
+    details: {
+      summary: "The classic pen-and-napkin pastime",
+      technologies: ["Node.js", "Express"]
+    },
+    image: "tic-tac-toe.png",
+    link: "https://slhodak.github.io/hrsf116-tic-tac-toe/public"
   }
 ];

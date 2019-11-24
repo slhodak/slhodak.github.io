62a3e4a06e7f9f92a211d6368b5ef80a0248b21b
Sam Hodak <sam.hodak@tact.ai>
2019-11-24 12:31:39 -0800

    update bio

--- bio.js
+++ bio.js
@@ -1,9 +1,9 @@
 const Bio = {
   location: "San Francisco Bay Area",
-  summary: "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;I'm a web developer with interests in information security, embedded systems, and augmented reality.",
-  personal: "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;In 2013 I earned a B.A. in Philosophy from Boston University, and I spent my early-mid 20s as a music producer and songwriter. I lived performed in Dallas, Nashville, and Kansas City. By 2018 I was spending less time on music, and knew that I would enjoy the art whether or not it was my source of income. I began coding for fun, and with curiosity for the new creative outlets it would open up. I soon realized I was spending all my free time at the keyboard, hacking and learning.",
-  personal_two: "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;I began with C# and Unity, then Swift, then Python, and then C++ and OpenGL. After a year of self-teaching, I decided to hire a bootcamp in order to tell me what <em>not</em> to study, and to give my interest the focus it would require to build a useful skillset. After researching the options, I attended the Hack Reactor Software Engineering Immersive in San Francisco.",
-  technical: "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;I've written persistent apps with backends relying on Postgres, Mongodb and Neo4j, and deployed them with AWS EC2 and Docker. I work mainly with web technologies such as React and Node, but I've also picked up C in order to better understand what's happening under the hood."
+  summary: "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;I'm a web developer with personal interests in information security, robotics, and augmented reality.",
+  personal: "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;In 2013 I earned a B.A. in Philosophy from Boston University, and I spent my early-mid 20s as a music producer and songwriter. Over the years I had coded for fun, and I eventually found myself spending all my free time hacking and learning.",
+  personal_two: "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;I began with C# and Unity, then tried Swift, Python, and C++. After a year of self-teaching, I enrolled in the Hack Reactor Software Engineering Immersive. I hired them in order to tell me what <strong>not</strong> to study, and to help me focus on building a useful, coherent skillset, which I'm happy to say they did extremely well.",
+  technical: "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;I've been a professional developer since the end of my time at Hack Reactor. I'm endlessly curious about software, and the little joke I like to make that my goal is to make sure my computer and I never have a reason to misunderstand each other."
 };
 
 export default Bio;
